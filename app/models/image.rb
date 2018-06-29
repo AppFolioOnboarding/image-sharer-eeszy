538aa366da9653b5445783b2bb926d1b33920cd9
Zhaoyu Sun <zhaoyu.sun@appfolio.com>
2018-06-28 21:37:58 -0700

    zs - Add some tags to the images

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,4 +1,6 @@
 class Image < ApplicationRecord
+  acts_as_taggable
+  acts_as_taggable_on :skills, :interests
   validates :link, presence: true,
                    format: URI.regexp(%w[http https])
 end
